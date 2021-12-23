35eb540d9fd7b27a94ef64b5d9f52b3f59c97080
Sean Mairs <secmairs@gmail.com>
2021-12-22 17:04:26 -0800

    created index files, began making button for generating bars

---
+++ index.js
@@ -0,0 +1,20 @@
+const bar = document.getElementsByClassName('bar');
+const barHolder = document.getElementById('bar-holder');
+console.log(bar.length)
+
+for (let i = 0; i < bar.length; i++) {
+    console.log(bar[i].style.backgroundColor);
+}
+
+function addBar() {
+    var num = document.getElementById("myNumber").value;
+    if (num > 50) {
+        num = 50;
+        document.getElementById("myNumber").value = 50;
+    }
+    if (num < 1) {
+        num = 1;
+        document.getElementById("myNumber").value = 1;
+    }
+    
+}
